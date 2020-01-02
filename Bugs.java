83dfb4d3ef6693201e3249b6c4b26543d49981b0
abolfazltaghavi <52733479+abolfazltaghavi@users.noreply.github.com>
2020-01-02 23:40:10 +0330

    Add files via upload

---
+++ Bugs.java
@@ -0,0 +1,6 @@
+public class Bugs {
+    // class plant // line 158 // poer of bullet forgoten
+    // implement the price and rest time of 152handler
+    // select in rail game and remove the cards
+    /// check the money and row in zombie game
+}
